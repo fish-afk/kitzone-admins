5554e78dabb584fb7f711f77f17d45d4bc7a62d7
SIHAAB <mirzashihab2@gmail.com>
2022-02-25 07:32:49 +0200

    initial

---
+++ src/root/Main_page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Template from './template'
+
+export default function Main_page() {
+  return (
+    <div>Main_page</div>
+  )
+}
---
+++ src/root/template.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Template(props) {
+
+
+  return (
+    <div>
+
+
+
+
+
+    </div>
+  )
+}
